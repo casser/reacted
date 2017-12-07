af9b0be8fc0b13d479b178775ce876b3c3f691c1
Sergey Mamyan <sergey.mamyan@gmail.com>
2017-12-07 16:28:55 +0400

    Testing Code Sandbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,37 @@
-import * as React from './preact';
-import {render} from './preact';
-import {Component} from './preact';
+import * as React from "./preact";
+import { render } from "./preact";
+import { Component } from "./preact";
 
 class Hello extends Component<{ grish: string }> {
-    render() {
-        console.info(this);
-        return <div>Hello</div>;
-    }
+  render() {
+    console.info(this);
+    return <div>Mello</div>;
+  }
 }
 
-function Other(this: any, props:any) {
-    console.info(this);
-    return <div>#{...props.children}#</div>;
+function Other(this: any, props: any) {
+  console.info(this);
+  return <div>#{props.children}#</div>;
 }
 
 const styles = {
-    fontFamily: 'sans-serif',
-    textAlign: 'center'
+  fontFamily: "sans-serif",
+  textAlign: "center"
 };
 
 const App = () => (
-    <div style={styles}>
-        <Hello grish={'Hello'}>
-            <a>Hello</a>
-            Hello World
-            <a>Hello</a>
-        </Hello>
-        <Other hehe="Hello">One{'Hello'}Two</Other>
-        <h2>Start Application</h2>
-    </div>
+  <div style={styles}>
+    <Hello grish={"Hello"}>
+      <a>Hello</a>
+      Hello World
+      <a>Hello</a>
+    </Hello>
+    <Other hehe="Hello">One{"Hello"}Two</Other>
+    <h2>Start Application</h2>
+  </div>
 );
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 if (root) {
-    render(<App/>, root);
+  render(<App />, root);
 }
